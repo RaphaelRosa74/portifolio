34a39910394151e32ac5f7ae046adcacd5a5f1bc
Raphael de Faria Rosa <99773236+RaphaelRosa74@users.noreply.github.com>
2025-09-03 14:00:21 -0300

    mudando o estilo de meu portifolio

--- script.js
+++ script.js
@@ -4,17 +4,10 @@ const skillsData = {
         description: "Possuo competências sólidas na linguagem, adquiridas tanto por meio de cursos especializados quanto por meio de projetos pessoais, os quais estão disponíveis para visualização em meu perfil no GitHub. Essa experiência consolidou minha proficiência na utilização eficaz da ferramenta, destacando minha capacidade de aplicar conceitos e resolver problemas de forma eficiente e estruturada.",
         link: "https://github.com/RaphaelRosa74/ByteBank"
     },
-    sql: {
-        description: "Tenho conhecimentos básicos em SQL, com experiência em criação e manipulação de bancos de dados. Participei de projetos que envolviam consultas complexas e otimização de queries.",
-        link: "https://github.com/seugithub/projeto-sql"
-    },
-    html: {
-        description: "Possuo habilidades em HTML, com experiência na criação de páginas web responsivas e semânticas. Estou sempre buscando aprender mais sobre as melhores práticas e novas tecnologias.",
+
+    htmlcss: {
+        description: "Tenho habilidades em HTML e CSS, com experiência na criação de páginas web semânticas, responsivas e com estilização moderna. Utilizo técnicas como Flexbox e Grid para desenvolver layouts atraentes e funcionais, e estou sempre em busca de aprender mais sobre as melhores práticas e novas tecnologias.",
         link: "https://github.com/RaphaelRosa74/projeto-social"
-    },
-    css: {
-        description: "Tenho conhecimentos em CSS, com experiência em estilização de páginas web e criação de layouts responsivos. Utilizo técnicas modernas como Flexbox e Grid para criar designs atraentes e funcionais.",
-        link: "https://github.com/RaphaelRosa74/projeto-login"
     },
     javascript: {
         description: "Possuo conhecimentos intermediários em JavaScript, com experiência em desenvolvimento de funcionalidades interativas para páginas web. Estou sempre buscando aprimorar minhas habilidades em frameworks como React e Node.js.",
@@ -22,7 +15,7 @@ const skillsData = {
     },
     react: {
         description: "Possuo conhecimentos em domínio em React com foco em componentes reutilizáveis, gerenciamento de estado (Context API/hooks), integração com APIs REST e otimização com hooks modernos",
-        link: "https://github.com/RaphaelRosa74/projeto-costs"
+        link: "https://github.com/RaphaelRosa74/miniblog"
     },
 };
 
